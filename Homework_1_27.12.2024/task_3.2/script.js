77c3090cd5d6a63b325133a68eebb157e43fe887
Lialina Anna <Lialina2010@ukr.net>
2025-01-12 19:45:52 +0200

    added new file in second folder

---
+++ Homework_1_27.12.2024/task_3.2/script.js
@@ -0,0 +1,5 @@
+let firstName = prompt("Whats your first name");
+let lastName = prompt("Whats your last name");
+let age = prompt("How old are your");
+
+console.log(`${firstName} ${lastName} ${age}`);
