d040a694401e75de4a943a89bd80584d02355581
Lialina Anna <Lialina2010@ukr.net>
2025-01-12 19:41:56 +0200

    added new file

---
+++ Homework_1_27.12.2024/task_3.1/script.js
@@ -0,0 +1,24 @@
+const undef = undefined;
+console.log(typeof undef);
+
+let value = null;
+console.log(typeof value);
+
+const number = 30;
+console.log(typeof number);
+
+const stroke = "Hello world";
+console.log(typeof stroke);
+
+const bool = true;
+console.log(typeof bool);
+
+const nulish = null;
+console.log(typeof nulish);
+
+const symb = Symbol("uniqID");
+console.log(typeof symb);
+
+const bigInteger = 123456789012345678901234567890n;
+console.log(typeof bigInteger);
+``;
