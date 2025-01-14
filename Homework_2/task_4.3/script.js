dd04ebd79681a84677826dc3488cb627d1c4bbec
Lialina Anna <Lialina2010@ukr.net>
2025-01-14 12:16:18 +0200

    added new task

--- Homework_2/task_4.3/script.js
+++ Homework_2/task_4.3/script.js
@@ -1,3 +1,45 @@
-let birthDate = prompt("What is your date of birth?");
-let liveCity = prompt("What city do you live in?");
-let kindSport = prompt("What kind of sport do you like?");
+let birthYear = prompt("Вкажіть Ваш рік народження");
+
+if (!birthYear) {
+  alert("Шкода, що ви не захотіли ввести свій рік народження");
+}
+
+let city = prompt("В якому місті ви живете");
+
+if (!city) {
+  alert("Шкода, що ви не захотіли ввести своє місто");
+}
+
+let kindSport = prompt("Який вид спорту улюблений?");
+
+if (!kindSport) {
+  alert("Шкода, що ви не захотіли ввести свій улюблений вид спорту");
+}
+
+if (birthYear && city && kindSport) {
+}
+
+const currentYear = new Date().getFullYear();
+const age = currentYear - parseInt(birthYear);
+
+let cityMessage;
+if (city.toLowerCase() === "Київ") {
+  cityMessage = "Ти живешь в столиці України";
+} else if (city.toLowerCase() === "Вашингтон") {
+  cityMessage = "Ти живешь в столиці США";
+} else if (city.toLowerCase() === "Лондон") {
+  cityMessage = "Ти живешь в столиці Великобританії";
+} else {
+  cityMessage = `Ти живешь у місті ${city}.`;
+}
+
+let sportMessage;
+if (kindSport.toLowerCase() === "футбол") {
+  sportMessage = "Круто! Ти хочешь стати як Ліонель Мессі?";
+} else if (kindSport.toLowerCase() === "бокс") {
+  sportMessage = "Круто! Ти хочешь стати як Олександр Усік?";
+} else if (kindSport.toLowerCase() === "фехтування") {
+  sportMessage = "Круто! Ти хочешь стати як Ольга Харлан?";
+}
+
+alert(`Ваш вік: ${age}\n${cityMessage}\n${sportMessage}`);
