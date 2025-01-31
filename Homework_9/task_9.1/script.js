7334eee5016ad66b9a50f34dfcea074280fd778e
Lialina Anna <Lialina2010@ukr.net>
2025-01-31 15:09:50 +0200

    added new folders and create task 9.1

---
+++ Homework_9/task_9.1/script.js
@@ -0,0 +1,34 @@
+let company = {
+  sales: [
+    { name: "John", salary: 1000 },
+    { name: "Alice", salary: 600 },
+    { name: "Anna", salary: 2000 },
+  ],
+
+  development: {
+    web: [
+      { name: "Peter", salary: 2000 },
+      { name: "Alex", salary: 1800 },
+    ],
+
+    internals: [
+      { name: "Jack", salary: 1300 },
+      { name: "Tom", salary: 1000 },
+    ],
+  },
+};
+
+function sumSalaries(department) {
+  if (Array.isArray(department)) {
+    return department.reduce((sum, employee) => sum + employee.salary, 0);
+  } else {
+    let sum = 0;
+    for (let subdep of Object.values(department)) {
+      sum += sumSalaries(subdep);
+    }
+    return sum;
+  }
+}
+
+let totalSalaries = sumSalaries(company);
+console.log(totalSalaries);
