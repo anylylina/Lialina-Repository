c402bbd76f9fd3dc61141c347e1966392355fe61
Lialina Anna <Lialina2010@ukr.net>
2025-04-22 19:46:07 +0300

    added new lesson

---
+++ Homework_25/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import "./index.css";
+import ClassComponent from './Components/ClassComponent'
+
+
+const App = () => {
+  return <ClassComponent />;
+};
+
+export default App;
