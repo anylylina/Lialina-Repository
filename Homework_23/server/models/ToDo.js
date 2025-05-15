acb6cd2bbbc1006ece5a7e0590acb3a8c2eda4c9
Lialina Anna <Lialina2010@ukr.net>
2025-05-15 17:30:07 +0300

    added changes in task 23

--- Homework_23/server/models/ToDo.js
+++ Homework_23/server/models/ToDo.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
   text: { type: String, required: true },
+  completed: { type: Boolean, default: false },
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
